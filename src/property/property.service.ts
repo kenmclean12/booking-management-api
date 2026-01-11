fd29ac5dfe58cda4a06a96fd7e54ae013353ea98
kenmclean12 <kenmclean20@gmail.com>
2026-01-11 15:47:48 -0800

    added service methods for property

--- src/property/property.service.ts
+++ src/property/property.service.ts
@@ -1,7 +1,99 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import {
+  PropertyCreateDto,
+  PropertyResponseDto,
+  PropertyUpdateDto,
+} from './dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class PropertyService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async findOne(id: number): Promise<PropertyResponseDto> {
+    const property = await this.prisma.property.findUnique({
+      where: { id },
+      include: { owner: true },
+    });
+
+    if (!property) {
+      throw new NotFoundException(`No property found with provided ID: ${id}`);
+    }
+
+    return property as PropertyResponseDto;
+  }
+
+  async findAll(): Promise<PropertyResponseDto[]> {
+    return (await this.prisma.property.findMany({
+      include: { owner: true },
+    })) as PropertyResponseDto[];
+  }
+
+  async findByUserId(id: number): Promise<PropertyResponseDto[]> {
+    const properties = await this.prisma.property.findMany({
+      where: { ownerId: id },
+      include: { owner: true },
+    });
+
+    if (!properties) {
+      throw new NotFoundException(
+        `No property found with the provided ID: ${id}`,
+      );
+    }
+
+    return properties as PropertyResponseDto[];
+  }
+
+  async create(dto: PropertyCreateDto): Promise<PropertyResponseDto> {
+    const created = await this.prisma.property.create({ data: dto });
+    if (!created) {
+      throw new BadRequestException(
+        `Could not create property entry with provided data: ${JSON.stringify(dto)}`,
+      );
+    }
+
+    return created as PropertyResponseDto;
+  }
+
+  async update(
+    id: number,
+    dto: PropertyUpdateDto,
+  ): Promise<PropertyResponseDto> {
+    const existing = await this.prisma.property.findUnique({ where: { id } });
+    if (!existing) {
+      throw new NotFoundException(`No user found with the provided ID: ${id}`);
+    }
+
+    const updated = await this.prisma.property.update({
+      where: { id },
+      data: dto,
+    });
+
+    if (!updated) {
+      throw new BadRequestException(
+        `Failed to update user ID: ${id} with provided data: ${JSON.stringify(dto)}`,
+      );
+    }
+
+    return updated as PropertyResponseDto;
+  }
+
+  async remove(id: number): Promise<PropertyResponseDto> {
+    const existing = await this.prisma.property.findUnique({ where: { id } });
+    if (!existing) {
+      throw new NotFoundException(
+        `No existing property found with provided ID: ${id}`,
+      );
+    }
+
+    await this.prisma.property.delete({ where: { id } });
+    return existing as PropertyResponseDto;
+  }
 }
